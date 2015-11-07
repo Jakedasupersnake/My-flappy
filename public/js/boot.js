79512c75cd42991c91631fd19b472ccb9deaae1c
Jakob Bassinger <jemoryb@gmail.com>
2015-11-07 11:59:39 -0500

    save

---
+++ public/js/boot.js
@@ -0,0 +1,19 @@
+// State Flappy.Boot
+
+'use strict';
+
+var Flappy = {  };
+
+ Flappy.Boot = function () {};
+
+Flappy.Boot.prototype = {
+
+  init: function () {
+  };
+
+  preload: function () {
+  };
+
+  create: function () {
+    this.game.stage.backgroundColor = 
+
